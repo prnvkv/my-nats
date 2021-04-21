b00d0d1b216b47251c60c323affd00e3f85ff536
Pranav Kamath KV <pranav.kamathkv@IN-GN-53225.local>
2021-04-21 12:04:25 +0530

    With Jetstream for queueing and pubsub

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.1 // indirect
-	github.com/nats-io/nats-server/v2 v2.2.0 // indirect
-	github.com/nats-io/nats.go v1.10.1-0.20210228004050-ed743748acac
+	github.com/nats-io/jwt v1.2.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.1.9 // indirect
+	// github.com/nats-io/nats.go v1.10.0
+	github.com/nats-io/nats.go v1.10.1-0.20210409153801-b8530c789d0b
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -6,6 +6,10 @@ package util
 
 import (
 	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	log "github.com/sirupsen/logrus"
 )
 
 // GetEnv read the value from the environment using a key and if no key present uses the fallback default value
@@ -14,4 +18,23 @@ func GetEnv(key, fallback string) string {
 		return value
 	}
 	return fallback
+}
+
+// setupConnOptions sets the connection options
+func SetupConnOptions(opts []nats.Option) []nats.Option {
+	totalWait := 10 * time.Second
+	reconnectDelay := time.Second
+
+	opts = append(opts, nats.ReconnectWait(reconnectDelay))
+	opts = append(opts, nats.MaxReconnects(int(totalWait/reconnectDelay)))
+	opts = append(opts, nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+		log.Printf("Disconnected due to: %s, will attempt reconnects for %.0fs", err, totalWait.Minutes())
+	}))
+	opts = append(opts, nats.ReconnectHandler(func(nc *nats.Conn) {
+		log.Printf("Reconnected [%s]", nc.ConnectedUrl())
+	}))
+	opts = append(opts, nats.ClosedHandler(func(nc *nats.Conn) {
+		log.Fatalf("Exiting: %v", nc.LastError())
+	}))
+	return opts
 }
